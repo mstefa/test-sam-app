1757ae195f3c942b9371139a00a72085b62a4d9f
mstefa <mstefanutti24@gmail.com>
2024-01-23 12:11:06 +0100

    feat: add a dynamoDb

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -1,4 +1,12 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
+
+const client = new DynamoDBClient({});
+
+const dynamo = DynamoDBDocumentClient.from(client);
+
+const tableName = process.env.SAMPLE_TABLE;
 
 /**
  *
@@ -17,6 +25,49 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
             body: JSON.stringify({
                 message: 'hello world',
             }),
+        };
+    } catch (err) {
+        console.log(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'some error happened',
+            }),
+        };
+    }
+};
+
+export const postProductsHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    try {
+        if (!event.body) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({
+                    message: 'no body',
+                }),
+            };
+        }
+
+        const requestJSON = JSON.parse(event.body);
+
+        await dynamo.send(
+            new PutCommand({
+                TableName: tableName,
+                Item: {
+                    id: requestJSON.id,
+                    price: requestJSON.price,
+                    name: requestJSON.title,
+                    description: requestJSON.description,
+                    image: requestJSON.image,
+                    category: requestJSON.category,
+                },
+            }),
+        );
+        const body = `Put item ${requestJSON.id}`;
+
+        return {
+            statusCode: 200,
+            body,
         };
     } catch (err) {
         console.log(err);
